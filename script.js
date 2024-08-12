ac564270a628c708569bc94a8f7748507f821311
MukeshHP03 <mukeshhadlyparamesh@gmail.com>
2024-08-12 16:05:55 +0530

    resetGame functionality added

--- script.js
+++ script.js
@@ -3,6 +3,41 @@ const cells = Array.from({length: 9}).fill(null);
 let winner = null;
 let currentPlayer = 'X';
 
+function handleCellClick(index){
+    if(winner || cells[index]) return;
+    cells[index] = currentPlayer;
+    render();
+    winner = checkWinner();
+    if(winner){
+        setTimeout(() => {
+            alert(`The Winner is ${winner}`);
+           resetGame();
+        }, 100)
+    }
+    else if(!cells.includes(null)){
+        setTimeout(() => {
+            alert(`It's a Tie`);
+           resetGame();
+        }, 100)
+
+    }
+    else currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+}
+
+function checkWinner(){
+    const winnerConditions = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8],
+        [0, 3, 6], [1, 4, 7], [2, 5, 8],
+        [0, 4, 8], [2, 4, 6]
+    ]
+    for(let conditions of winnerConditions){
+        const[a, b, c] = conditions;
+        if(cells[a] && cells[a] === cells[b] && cells[a] === cells[c]) 
+            return cells[a];
+    }
+    return null;
+}
+
 function render(){
     board.innerHTML = '';
     cells.forEach((value, index) => {
@@ -13,3 +48,11 @@ function render(){
         board.appendChild(cell);
     })
 }
+
+function resetGame(){
+    cells.fill(null);
+    winner = null;
+    currentPlayer = 'X';
+    render();
+}
+render();
